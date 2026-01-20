8c57ddc4bc42f45c521b7e0327b1abe9b723e86e
victoria <victoquiroga@gmail.com>
2026-01-20 15:43:08 -0300

    Instalacion de Capacitor y Haptics configurada

--- frontend/capacitor.config.ts
+++ frontend/capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.victoria.elviajero',
+  appId: 'com.elviajero.app',
   appName: 'El Viajero',
   webDir: 'dist'
 };
