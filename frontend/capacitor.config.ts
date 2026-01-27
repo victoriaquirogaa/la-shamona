ae77257a0878dfe2aaebeb09df0ae14ba947fb4a
victoria <victoquiroga@gmail.com>
2026-01-27 19:52:47 -0300

    funciona iniciar con gmail

--- frontend/capacitor.config.ts
+++ frontend/capacitor.config.ts
@@ -1,9 +1,19 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.elviajero.app',
+  appId: 'com.victoria.elviajero',
   appName: 'El Viajero',
-  webDir: 'dist'
+  webDir: 'dist', // O 'build' dependiendo de tu proyecto
+  server: {
+    androidScheme: 'https'
+  },
+  plugins: {
+    GoogleAuth: {
+      scopes: ['profile', 'email'],
+      serverClientId: '930301862044-855blh0lde4i34m4vti3eubh80233n8d.apps.googleusercontent.com', // <--- BUSCAR ESTO
+      forceCodeForRefreshToken: true,
+    },
+  },
 };
 
 export default config;
